699ceae0dea388d6c02d30cb94760ebb9b67872a
franzimbi <fzimbimbakis@fi.uba.ar>
2023-09-03 18:30:06 -0300

    arrancamos

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -1,1 +1,7 @@
-print("hola")
+# https://jamboard.google.com/d/1NkHMri_u8HTwly7UPNdU_GuMhs3_AFTz474NmIDRI9w/viewer?f=0
+tareas = ["Cocinar", "Primeros auxilios",
+          "Meteorología", "astronomía", "Electricidad", "Mecanico", "Programador"]
+candidatos = [["R.J. MacReady", 1, 5, 6], ["Nauls", 1, 2], ["Childs", 3, 7],
+              ["Dr. Copper", 2, 4], ["George Bennings", 1, 3], ["Garry", 2, 6, 3]]
+
+dicCandidatos = {}
