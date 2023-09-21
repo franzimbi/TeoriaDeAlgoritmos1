d2b9d305e89ff10e861a56e18b42288675810094
franzimbi <fzimbimbakis@fi.uba.ar>
2023-09-21 18:19:37 -0300

    listo!

--- tp1/personal.py
+++ tp1/personal.py
@@ -45,7 +45,7 @@ def branchAndBound(candidatos, puestos):
                 mejorSolucion = s.copy()
         return mejorSolucion
 
-    candidatosOrdenados = ordenarCandidatos(candidatos, len(tareas))
+    candidatosOrdenados = ordenarCandidatos(candidatos, len(puestos))
     puestosCubiertos = set()
     mejorSolucion = []
     for i in candidatosOrdenados:
@@ -54,12 +54,6 @@ def branchAndBound(candidatos, puestos):
 
 
 ######### MAIN #############
-
-tareas = ["Cocinar", "Primeros auxilios",
-              "Meteorología", "astronomía", "Electricidad", "Mecanico", "Programador"]
-candidatos = [["R.J. MacReady", 1, 5, 6], ["Nauls", 1, 2], ["Childs", 3, 7],
-                  ["Dr. Copper", 2, 4], ["George Bennings", 1, 3], ["Garry", 2, 6, 3]]
-
 argumentos = sys.argv
 if len(argumentos) != 3:
     print("ERROR")
