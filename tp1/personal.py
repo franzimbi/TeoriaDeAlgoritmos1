3a8313cfb63fd3ec328cb43b9ee6d1daaf813b9f
franzimbi <fzimbimbakis@fi.uba.ar>
2023-09-21 00:25:37 -0300

    ejercicio 1 y 3

--- tp1/personal.py
+++ tp1/personal.py
@@ -5,49 +5,30 @@ candidatos = [["R.J. MacReady", 1, 5, 6], ["Nauls", 1, 2], ["Childs", 3, 7],
               ["Dr. Copper", 2, 4], ["George Bennings", 1, 3], ["Garry", 2, 6, 3]]
 
 #[ childs, cooper, macready]
-
-class PuestosCubiertos:
-    def __init__(self, listaPuestos):
-        self._puestos = {}
-        self._tam = 0
-        for i in listaPuestos:
-            self._puestos[i] = 0
-
-    def cubrirPuestos(self, puestos):
-        for i in puestos:
-            if i not in self._puestos:
-                return False
-            if self._puestos[i] == 0:
-                self._tam +=1
-            self._puestos[i] += 1
-        return  True
-
-    def liberarPuestos(self, puestos):
-        for i in puestos:
-            if i not in self._puestos:
-                return False
-            if self._puestos[i] == 0:
-                continue
-            self._puestos[i] -= 1
-            if self._puestos[i] == 0:
-                self._tam -=1
-        return True
-
-    def puestosCubiertos(self):
-        aux = []
-        for i in self._puestos:
-            if self._puestos[i] > 0:
-                aux.append(i)
-        return aux
-
-    def __len__(self):
-        return self._tam
-
-    def __str__(self):
-        return str(self.puestosCubiertos())
-
-
-
+def ordenarCandidatos(candidatos, cantidadTareas):  # O(C * T)
+    ordenPorCantidad = list([] for _ in range(cantidadTareas)) # O(T) t: cantidad de tareas
+    contador = [0]*cantidadTareas # O(n) n: cantidad de candidatos
+    puestosUnicos = set()
+    for candidato in candidatos: #O (n)
+        ordenPorCantidad[len(candidato)-1].append(candidato)
+        for tarea in candidato:
+            if tarea!=candidato[0]:
+                contador[tarea-1] += 1
+    for i in range(len(contador)): # O(T)
+        if contador[i] == 1:
+            puestosUnicos.add(i+1)
+    res = []
+    for i in range(len(ordenPorCantidad)-1, 0 , -1):  # O(n)
+        for candidato in ordenPorCantidad[i]: # O(T)
+            agregado = False
+            for c in candidato:
+                if c in puestosUnicos:
+                    res.insert(0, candidato)
+                    agregado = True
+                    break
+            if not agregado:
+                res.append(candidato)
+    return res
 def branchAndBound(candidatos, puestos):
     def _bAb(indice, candidatos, puestosCubiertos, solucionActual, mejorSolucion, cantidadPuestos):
         if len(puestosCubiertos) == cantidadPuestos:
@@ -70,11 +51,12 @@ def branchAndBound(candidatos, puestos):
                 mejorSolucion = s.copy()
         return mejorSolucion
 
+    candidatosOrdenados = ordenarCandidatos(candidatos, len(tareas))
     puestosCubiertos = set()
     mejorSolucion = []
-    for i in candidatos:
+    for i in candidatosOrdenados:
         mejorSolucion.append(i[0])
-    return  _bAb(0, candidatos, puestosCubiertos, [], mejorSolucion, len(puestos))
+    return  _bAb(0, candidatosOrdenados, puestosCubiertos, [], mejorSolucion, len(puestos))
 
 inicio = time.time()
 
