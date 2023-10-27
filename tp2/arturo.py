b3b1a62cb5fc8d92be6f1182390e872f969ef6a5
franzimbi <fzimbimbakis@fi.uba.ar>
2023-10-27 19:12:21 -0300

    arturo elegi facil flaco

--- tp2/arturo.py
+++ tp2/arturo.py
@@ -1,6 +1,7 @@
+import sys
+
 def valorCaballero(caballero):
     return caballero[1]
-
 def nombreCaballero(caballero):
     return caballero[0]
 def encontrarElUltimoNegativo(caballeros):
@@ -20,27 +21,41 @@ def encontrarMaximo(arr):
 def construirCamino(optimos, caballeros):
     resultado = []
     pos = encontrarMaximo(optimos)
-    for i in range(pos, -1, -1):
-
-
+    while pos > 0 :
+        resultado.append(caballeros[pos-1][0])
+        pos -= 1
+    return resultado
 
 def caballerosMasPopulares(caballeros):
     ultimoNegativo = encontrarElUltimoNegativo(caballeros)
     caballerosReordenados = []
     if ultimoNegativo != -1:
-        caballerosReordenados.extend(caballeros[ultimoNegativo:])
+        caballerosReordenados.extend(caballeros[ultimoNegativo+1:])
         caballerosReordenados.extend(caballeros[:ultimoNegativo])
     else:
         caballerosReordenados = caballeros
     optimos = []
     optimos.append(0)
-    for i in range(len(caballerosReordenados)-1):
+    for i in range(len(caballerosReordenados)):
         valor = valorCaballero(caballerosReordenados[i])
         if optimos[i] + valor >= valor:
             optimos.append(optimos[i] + valor)
         else:
             optimos.append(valor)
-    return construirCamino(optimos, caballerosReordenados)
+    return construirCamino(optimos, caballerosReordenados)[::-1]
 
+# ___ MAIN ___
+argumentos = sys.argv
+if len(argumentos) != 2:
+    print("ERROR")
+    sys.exit()
 
+with open(argumentos[1], 'r') as archivo:
+    lineas = archivo.readlines()
 
+listaCaballeros = []
+for l in lineas:
+    elementos = l.strip().split(',')
+    listaCaballeros.append([elementos[0], int(elementos[1])])
+
+print(caballerosMasPopulares(listaCaballeros))
